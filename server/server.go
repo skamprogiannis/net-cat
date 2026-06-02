d3141c66ec6bb9428db9aeaa66a702899032183c
Daniel <danieltymoshenko@gmail.com>
2026-06-02 16:40:10 +0300

    tcp listener with net.listen

--- server/server.go
+++ server/server.go
@@ -1,1 +1,23 @@
 package server
+
+import (
+	"log"
+	"net"
+)
+
+func Start(port string) {
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Connection error:", err)
+			continue
+		}
+		_ = conn
+	}
+}
