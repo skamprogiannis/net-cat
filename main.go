872c0b25d2eda81b02ec781a29dc52fda58da74e
Daniel <danieltymoshenko@gmail.com>
2026-06-02 16:39:01 +0300

    parse port argument from os.args

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	port := "8989"
+
+	if len(os.Args) == 2 {
+		port = os.Args[1]
+	} else if len(os.Args) > 2 {
+		fmt.Println("[USAGE]: ./TCPChat $port")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Listening on the port :%s\n", port)
+}
