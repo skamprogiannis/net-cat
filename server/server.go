cd53a37ffdf384700fa2179062c5144bf9b8354a
Daniel <danieltymoshenko@gmail.com>
2026-06-09 09:42:21 +0300

    add global clients slice / add remove helpers

--- server/server.go
+++ server/server.go
@@ -82,3 +82,20 @@ func sendWelcomePrompt(conn net.Conn) error {
 	_, err := fmt.Fprint(conn, welcomeMessage)
 	return err
 }
+
+func addClient(c *Client) {
+	mu.Lock()
+	clients = append(clients, c)
+	mu.Unlock()
+}
+
+func removeClient(c *Client) {
+	mu.Lock()
+	for i, client := range clients {
+		if client == c {
+			clients = append(clients[:i], clients[i+1:]...)
+			break
+		}
+	}
+	mu.Unlock()
+}
